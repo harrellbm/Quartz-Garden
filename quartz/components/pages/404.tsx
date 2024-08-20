efd187c4882cd707eea5b245c31730a7e70f66a4
harrellbm <brendenharrell1@gmail.com>
2024-08-19 21:17:29 -0500

    404 message

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -9,6 +9,7 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   return (
     <article class="popover-hint">
       <h1>404</h1>
+      <h2>This Page does't exist, or I haven't Uploaded it yet!</h2>
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
       <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
     </article>
