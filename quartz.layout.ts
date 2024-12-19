b660748218390969e54a5678381ed5f73dc31131
Harrellbm <brendenharrell1@gmail.com>
2024-12-19 11:35:15 -0800

    tweak graph forces

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -50,22 +50,54 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.DesktopOnly(Component.Graph()),
     Component.Backlinks(), 
-    Component.RecentNotes({title:"Recent Thoughts", showTags: false})
+    Component.DesktopOnly(Component.Graph({
+        localGraph: {
+          drag: true, // whether to allow panning the view around
+          zoom: true, // whether to allow zooming in and out
+          depth: 1, // how many hops of notes to display
+          scale: 1.5, // default view scale
+          repelForce: 0.5, // how much nodes should repel each other
+          centerForce: 0.3, // how much force to use when trying to center the nodes
+          linkDistance: 30, // how long should the links be by default?
+          fontSize: 0.9, // what size should the node labels be?
+          opacityScale: 1, // how quickly do we fade out the labels when zooming out?
+          removeTags: [], // what tags to remove from the graph
+          showTags: true, // whether to show tags in the graph
+        },
+        globalGraph: {
+          drag: true,
+          zoom: true,
+          depth: -1,
+          scale: 0.9,
+          repelForce: 2.5,
+          centerForce: 1.5,
+          linkDistance: 45,
+          fontSize: 0.6,
+          opacityScale: 0.9,
+          removeTags: [], // what tags to remove from the graph
+          showTags: true, // whether to show tags in the graph
+        },
+      })),
+    Component.RecentNotes({title:"Recent Thoughts", showTags: false}),
   ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMetaCustom(),],
+  beforeBody: [
+    Component.Breadcrumbs(), 
+    Component.ArticleTitle(), 
+    Component.ContentMetaCustom(),
+  ],
   left: [
-    Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
+    Component.MobileOnly(Component.Spacer()),
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     Component.Graph(),
+    Component.RecentNotes({title:"Recent Thoughts", showTags: false}),
   ],
 }
